c818880619f74b9495d1906e3bbc4938177e6802
macbookpro <melhemshoker@claims-express.net>
2023-08-21 14:33:33 +0300

    pod

--- lib/View/notess.dart
+++ lib/View/notess.dart
@@ -269,66 +269,91 @@ class _NotessViewState extends State<NotessView> {
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         _buildMultilineTextField(),
+                        //
+                        // Container(
+                        //   margin: const EdgeInsets.all(3),
+                        //   padding: const EdgeInsets.all(3),
+                        //   height: 80,
+                        //   width: double.infinity,
+                        //   alignment: Alignment.center,
+                        //   decoration: BoxDecoration(
+                        //     color: Color(0xFFFAF0E6),
+                        //     border: Border.all(
+                        //       color: Colors.indigo,
+                        //       width: 3,
+                        //     ),
+                        //   ),
+                        //   child: Row(children: [
+                        //     ElevatedButton(
+                        //       onPressed: getRecorderFn(),
+                        //       //color: Colors.white,
+                        //       //disabledColor: Colors.grey,
+                        //       child: Icon(_mRecorder!.isRecording ? Icons.stop : Icons.mic),
+                        //     ),
+                        //     SizedBox(
+                        //       width: 20,
+                        //     ),
+                        //     Text(_mRecorder!.isRecording
+                        //         ? 'Recording in progress'
+                        //         : 'Recorder is stopped'),
+                        //   ]),
+                        // ),
+                        // Container(
+                        //   margin: const EdgeInsets.all(3),
+                        //   padding: const EdgeInsets.all(3),
+                        //   height: 80,
+                        //   width: double.infinity,
+                        //   alignment: Alignment.center,
+                        //   decoration: BoxDecoration(
+                        //     color: Color(0xFFFAF0E6),
+                        //     border: Border.all(
+                        //       color: Colors.indigo,
+                        //       width: 3,
+                        //     ),
+                        //   ),
+                        //   child: Row(children: [
+                        //     ElevatedButton(
+                        //       onPressed: getPlaybackFn(),
+                        //       //color: Colors.white,
+                        //       //disabledColor: Colors.grey,
+                        //       child: Icon(_mPlayer!.isPlaying ? Icons.pause : Icons.play_arrow),
+                        //     ),
+                        //     SizedBox(
+                        //       width: 20,
+                        //     ),
+                        //     Text(_mPlayer!.isPlaying
+                        //         ? 'Playback in progress'
+                        //         : 'Player is stopped'),
+                        //   ]),
+                        // ),
 
-                        Container(
-                          margin: const EdgeInsets.all(3),
-                          padding: const EdgeInsets.all(3),
-                          height: 80,
-                          width: double.infinity,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            color: Color(0xFFFAF0E6),
-                            border: Border.all(
-                              color: Colors.indigo,
-                              width: 3,
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: getRecorderFn(),
+                                child: Icon(
+                                  _mRecorder!.isRecording
+                                      ? Icons.stop
+                                      : Icons.mic,
+                                ),
+                              ),
                             ),
-                          ),
-                          child: Row(children: [
-                            ElevatedButton(
-                              onPressed: getRecorderFn(),
-                              //color: Colors.white,
-                              //disabledColor: Colors.grey,
-                              child: Text(_mRecorder!.isRecording ? 'Stop' : 'Record'),
+                            SizedBox(width: 20),
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: getPlaybackFn(),
+                                child: Icon(
+                                  _mPlayer!.isPlaying
+                                      ? Icons.pause
+                                      : Icons.play_arrow,
+                                ),
+                              ),
                             ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Text(_mRecorder!.isRecording
-                                ? 'Recording in progress'
-                                : 'Recorder is stopped'),
-                          ]),
+                          ],
                         ),
-                        Container(
-                          margin: const EdgeInsets.all(3),
-                          padding: const EdgeInsets.all(3),
-                          height: 80,
-                          width: double.infinity,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            color: Color(0xFFFAF0E6),
-                            border: Border.all(
-                              color: Colors.indigo,
-                              width: 3,
-                            ),
-                          ),
-                          child: Row(children: [
-                            ElevatedButton(
-                              onPressed: getPlaybackFn(),
-                              //color: Colors.white,
-                              //disabledColor: Colors.grey,
-                              child: Text(_mPlayer!.isPlaying ? 'Stop' : 'Play'),
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Text(_mPlayer!.isPlaying
-                                ? 'Playback in progress'
-                                : 'Player is stopped'),
-                          ]),
-                        ),
-
-
-
                         SizedBox(
                           height: 20,
                           width: double.infinity,
